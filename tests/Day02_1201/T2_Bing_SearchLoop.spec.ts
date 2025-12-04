4b13fd21181090bc542b17f78749ec9296a0a821
Shafiat Siddiquee Arafat <shafiat825@gmail.com>
2025-12-04 16:27:54 -0500

    playwright 2025

--- tests/Day02_1201/T2_Bing_SearchLoop.spec.ts
+++ tests/Day02_1201/T2_Bing_SearchLoop.spec.ts
@@ -1,33 +1,33 @@
 import { Page, test } from '@playwright/test';
 
-//initalize page varriable for browser
-let page: Page
-let value: string //just an example
-let integer: number //just an example
+test("Search for a keyword on bing website @smoke", async ({ page }) => {
 
+    //declzre arraylist of Cars
+    //let cars: string[] = ['BMW', 'Audi', 'Mercedes', 'Toyota', 'Honda']
+    let cars = new Array<string>()
+    cars.push('BMW')
+    cars.push('Audi')
+    cars.push('Mercedes')
+    cars.push('Toyota')
+    cars.push('Honda')
 
-test.beforeAll(async ({ browser }) => {
-    //create a new page instance
-    page = await browser.newPage()
+    for (let i = 0; i < cars.length; i++) {
 
-})
-
-test("Search for a keyword on bing website @smoke", async () => {
     //navigate to bing
     await page.goto('https://www.bing.com')
     //type a keyword on the search box
-    await page.locator('[name="q"]').fill('Playwright')
+    await page.locator('[name="q"]').fill(cars[i])
     await page.waitForTimeout(2000) //wait for few seconds for the next step
     //click on the search icon
     await page.keyboard.press('Enter')
-})//end of test 1
 
-test("Capture Search", async() =>{
     //capture search result text
     let searchResult = await page.locator('[class = "sb_count"]').textContent()
     //for concatenation purpose use , or +
     console.log("Search result: " + searchResult)
     //extract number from the search result
     let resultsArray = searchResult?.split(' ')
-    console.log("Result Array "+ resultsArray[1])  
+    console.log("For car: " + cars[i]+ " : " + resultsArray[1]) 
+
+    }//end of for loop
 }) //end of test 2
